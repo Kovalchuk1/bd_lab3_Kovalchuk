e2319e37643f2d1bb9fb4251202352d4d0071d84
Kovalchuk1 <72132862+Kovalchuk1@users.noreply.github.com>
2022-12-27 02:55:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ group by genre_name
 
 query_3 = '''
 CREATE VIEW MoreAverageChannelViews AS
-select ch_name, ch_views from channel
-where ch_views >= (select  avg(ch_views) from  channel)
+select ch_name, views_v from channel
+join viewss using(id_views)
+where views_v >= (select  avg(views_v) from  viewss)
 '''
 
 conn = psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port)
@@ -43,6 +44,7 @@ with conn:
         x.append(row[0])
         y.append(row[1])
     plt.bar(x, y, width=0.1, alpha=0.6, color='yellow', edgecolor="k", linewidth=2)
+    plt.ylabel('Number of channels')
     plt.title('Number of channels in each country')
     plt.show()
 
@@ -65,7 +67,9 @@ with conn:
     cur.execute(query_3)
     cur.execute('SELECT * FROM MoreAverageChannelViews')
     for row in cur:
+        y.append(row[1])
         x.append(row[0])
-    plt.bar(range(len(x)), x, color='yellow', edgecolor="k")
+    plt.bar(x, y, color='yellow', edgecolor="k")
     plt.title('Channel views that are greater than the average number of views of all channels')
+    plt.ylabel('Number of views')
     plt.show()
