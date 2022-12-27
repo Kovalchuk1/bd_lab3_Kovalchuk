618e950f771edfc6cbc149e20fd6e000c3747f2a
Kovalchuk1 <72132862+Kovalchuk1@users.noreply.github.com>
2022-12-27 03:03:33 +0200

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -7,21 +7,19 @@ select * from channelcopy;
 DO $$
  DECLARE
      name_ch	  channelcopy.ch_name%TYPE;
-	 views_ch     channelcopy.ch_views%TYPE;
-	 ch_sub       channelcopy.ch_subscribers%TYPE;
+     views_ch     channelcopy.id_views%TYPE;
      id_genrech   channelcopy.id_genre%TYPE;
      id_countrych channelcopy.id_country%TYPE;
 
  BEGIN
  	 name_ch := 'channel-';
 	 views_ch := 2500;
-	 ch_sub := 100;
      id_genrech := 0;
      id_countrych := 1;
      FOR counter IN 1..10
          LOOP
-            INSERT INTO channelcopy (ch_name, ch_views, ch_subscribers, id_genre, id_country)
-             VALUES (name_ch || counter + 1, views_ch + counter, ch_sub - counter + 10,  counter + 1, counter + 1);
+            INSERT INTO channelcopy (ch_name, id_views, id_genre, id_country)
+             VALUES (name_ch || counter + 1, views_ch + counter,  counter + 1, counter + 1);
          END LOOP;
  END;
  $$
