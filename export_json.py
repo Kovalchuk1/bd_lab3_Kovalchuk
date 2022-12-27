d0e80a799a473bd9b88c598bab8cd11722f3bda1
Kovalchuk1 <72132862+Kovalchuk1@users.noreply.github.com>
2022-12-27 03:53:55 +0200

    Update export_json.py

--- export_json.py
+++ export_json.py
@@ -8,22 +8,30 @@ host = 'localhost'
 port = '5432'
 
 
+OUTPUT_FILE = 'all_data.json'
+
+TABLES = [
+    'channel',
+    'genre',
+    'country',
+    'viewss'
+]
+
 conn = psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port)
 
 data = {}
 with conn:
-
     cur = conn.cursor()
 
-    for table in ('channel', 'genre', 'country'):
+    for table in TABLES:
         cur.execute('SELECT * FROM ' + table)
         rows = []
         fields = [x[0] for x in cur.description]
 
-    for row in cur:
-        rows.append(dict(zip(fields, row)))
+        for row in cur:
+            rows.append(dict(zip(fields, row)))
 
-    data[table] = rows
+        data[table] = rows
 
-    with open('all_data.json', 'w') as outf:
-        json.dump(data, outf, default=str)
+with open(OUTPUT_FILE, 'w') as outf:
+    json.dump(data, outf, default=str, indent=4)
